aaea539e2dd787c0fe4f5033e27f0ba7e80949f2
Nga <felixwhite2008@hotmail.com>
2019-10-29 17:41:44 +0100

    novo20

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiUrl: 'https://ngangalixapi.herokuapp.com',
 
   tokenWhitelistedDomains: [ new RegExp('ngangalixapi.herokuapp.com') ],
-=======
-  apiUrl: 'https://algamoney-api-eben.herokuapp.com',
-
-  tokenWhitelistedDomains: [ new RegExp('algamoney-api-eben.herokuapp.com') ],
->>>>>>> 6c295d5763f0c6c1c9a2b538b974473aea9ef73d
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 
 };
